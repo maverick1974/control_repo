36e2feed4b043c235414bc3bbdaff0695f53e9b4
maverick1974 <mark.laverick@gmail.com>
2019-11-23 19:00:53 +0000

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,24 +1,24 @@
 class minecraft {
   file {‘/opt/minecraft’:
-    ensure => directory,
+    ensure      => directory,
   }
   file {‘/opt/minecraft/minecraft_server.1.12.2.jar’:
-    ensure => file,
+    ensure      => file,
     source      => 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar',
   }
   package {‘java’:
-    ensure => present,
+    ensure      => present,
   }
   file {‘/opt/minecraft/eula.txt’:
-    ensure => file,
-    content => ‘eula=true’
+    ensure      => file,
+    content     => ‘eula=true’
   }
   file {‘/etc/systemd/system/minecraft.service’:
-    ensure => file,
-    source => ‘puppet:///modules/minecraft/minecraft.service’,
+    ensure      => file,
+    source      => ‘puppet:///modules/minecraft/minecraft.service’,
   }
   service { ‘minecraft’:
-    ensure => running,
-    enable => true,
+    ensure      => running,
+    enable      => true,
   }
 }
