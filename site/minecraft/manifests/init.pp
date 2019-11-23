344a5b02d7198b7b9eeb7022877c5057a62c68b1
maverick1974 <mark.laverick@gmail.com>
2019-11-23 22:18:15 +0000

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,30 +1,31 @@
 class minecraft (
   $url = 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar',
   $install_dir = '/opt/minecraft'
-  $service_dir = '/etc/systemd/system/minecraft.service'
 ){
   file {$install_dir:
-    ensure      => directory,
+    ensure        => directory,
   }
   file {"${install_dir}/minecraft_server.jar":
-    ensure      => file,
-    source      => $url,
-    before      =>  Service['minecraft',
+    ensure        => file,
+    source        => $url,
+    before        =>  Service['minecraft',
   }
   package {'java':
-    ensure      => present,
+    ensure        => present,
   }
   file {"${install_dir}/eula.txt":
-    ensure      => file,
-    content     => 'eula=true'
+    ensure        => file,
+    content       => 'eula=true'
   }
-  file {'$service_dir:
-    ensure      => file,
-    source      => 'puppet:///modules/minecraft/minecraft.service',
+  file {'/etc/systemd/system/minecraft.service':
+    ensure        => file,
+    content       =>  epp('minecraft/minecraft.service',{
+      install_dir => $install_dir,
+    })
   }
   service { 'minecraft':
-    ensure      => running,
-    enable      => true,
-    require     => [Package['Java'],File["${install_dir}/eula.txt"],File[{$service_dir}]],
+    ensure        => running,
+    enable        => true,
+    require       => [Package['Java'],File["${install_dir}/eula.txt"],File['/etc/systemd/system/minecraft.service']],
   }
 }
